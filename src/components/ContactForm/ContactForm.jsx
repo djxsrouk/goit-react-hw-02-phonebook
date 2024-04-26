3ca4831951f2b93695bf47ecab929fb2ad3a6973
Andrei-Gabriel Paiu-Rotundu <114349768+djxsrouk@users.noreply.github.com>
2024-04-26 19:30:30 +0100

    commit reset form after submit

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -38,6 +38,7 @@ class ContactForm extends Component {
           type="text"
           name="name"
           placeholder='Name'
+          value={this.state.name}
           pattern="^[a-zA-Z]+(([' \-][a-zA-Z ])?[a-zA-Z]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           onChange={(event) => this.setState({name: event.target.value})}
@@ -50,6 +51,7 @@ class ContactForm extends Component {
             type="tel"
             name="number"
             placeholder='Phone number'
+            value={this.state.number}
             pattern= "\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             onChange={(event) => this.setState({number: event.target.value})}
